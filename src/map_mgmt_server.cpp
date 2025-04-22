19245a05527d02024624a37a6616e90828aa3166
wei.sun <sunweijm.sun@gmail.com>
2025-04-22 16:43:58 +0800

    增加 根据地图名称 删除地图文件

--- src/map_mgmt_server.cpp
+++ src/map_mgmt_server.cpp
@@ -5,29 +5,38 @@
 #include <string>
 #include <set>
 #include <ament_index_cpp/get_package_share_directory.hpp>
+#include "robot_interfaces/srv/get_maps_name_list.hpp"
+#include "robot_interfaces/srv/remove_map_file.hpp"
 
 /***
- * 1.获取当前maps的列表
- * 
+ * 1.获取当前maps目录下各个map name列表
+ * 2.传入地图名称，获取地图文件pgm、yaml文件
+ * 3.删除地图
  */
 using namespace std;
 class MapMgmtServer : public rclcpp::Node {
 public:
     MapMgmtServer() : Node("map_mgmt_server") {
-        service_ = this->create_service<std_srvs::srv::Trigger>(
+        std::string package_share_directory = ament_index_cpp::get_package_share_directory("map_server_extension");
+        maps_dir = package_share_directory + "/maps/";
+
+        service_gmnl_ = this->create_service<robot_interfaces::srv::GetMapsNameList>(
             "get_maps_name_list",
-            std::bind(&MapMgmtServer::handle_request, this, std::placeholders::_1, std::placeholders::_2));
-        RCLCPP_INFO(this->get_logger(), "Service is ready to get maps name list.");
+            std::bind(&MapMgmtServer::handle_request_gmnl, this, std::placeholders::_1, std::placeholders::_2));
+
+        service_rmf_ = this->create_service<robot_interfaces::srv::RemoveMapFile>(
+            "remove_map_file",
+            std::bind(&MapMgmtServer::handle_request_rmf, this, std::placeholders::_1, std::placeholders::_2));
+
+        RCLCPP_INFO(this->get_logger(), "Service is ready to map management.");
     }
 
 private:
-    void handle_request(
-        const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
-        std::shared_ptr<std_srvs::srv::Trigger::Response> response) {
+    void handle_request_gmnl(
+        const std::shared_ptr<robot_interfaces::srv::GetMapsNameList::Request> request,
+        std::shared_ptr<robot_interfaces::srv::GetMapsNameList::Response> response) {
         (void)request;  // Unused
 
-        std::string package_share_directory = ament_index_cpp::get_package_share_directory("map_server_extension");
-        std::string maps_dir = package_share_directory + "/maps/";
         std::set<std::string> pgm_files;
         std::set<std::string> yaml_files;
         std::vector<std::string> valid_map_names;
@@ -35,7 +44,7 @@ private:
         // Read directory
         DIR *dir = opendir(maps_dir.c_str());
         if (dir == nullptr) {
-            response->success = false;
+            response->result = false;
             response->message = "Failed to open maps directory.";
             RCLCPP_ERROR(this->get_logger(), "Directory not found: %s", maps_dir.c_str());
             return;
@@ -69,14 +78,42 @@ private:
         }
 
         // Prepare response
-        response->success = true;
-        response->message = "";
+        response->result = true;
+        response->name_list = "";
         for (const auto &name : valid_map_names) {
-            response->message += name + ";";
+            response->name_list += name + ";";
         }
     }
 
-    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr service_;
+    void handle_request_rmf(
+        const std::shared_ptr<robot_interfaces::srv::RemoveMapFile::Request> request,
+        std::shared_ptr<robot_interfaces::srv::RemoveMapFile::Response> response) {
+
+        std::string map_name = request->map_name;
+        
+        // Construct file paths
+        std::string pgm_path = maps_dir + map_name + ".pgm";
+        std::string yaml_path = maps_dir + map_name + ".yaml";
+        // Delete PGM file
+        if (std::remove(pgm_path.c_str()) != 0) {
+            response->result = false;
+            response->message = "Failed to delete PGM file: " + map_name + ".pgm";
+            return;
+        }
+        // Delete YAML file
+        if (std::remove(yaml_path.c_str()) != 0) {
+            response->result = false;
+            response->message = "Failed to delete YAML file: " + map_name + ".yaml";
+            return;
+        }
+        response->result = true;
+        response->message = "Successfully deleted map files: " + map_name + ".pgm and " + map_name + ".yaml";
+    }
+
+    // rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr service_;
+    rclcpp::Service<robot_interfaces::srv::GetMapsNameList>::SharedPtr service_gmnl_;
+    rclcpp::Service<robot_interfaces::srv::RemoveMapFile>::SharedPtr service_rmf_;
+    std::string maps_dir;
 };
 
 int main(int argc, char **argv) {
